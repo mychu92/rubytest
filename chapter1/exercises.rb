313445865a57c427c3dba6e463a364991e92446c
Maggie Chu <mychu92@gmail.com>
2016-02-04 19:32:49 +0800

    ex5

--- chapter1/exercises.rb
+++ chapter1/exercises.rb
@@ -1,11 +1,7 @@
 #Ex1:
 "Maggie" + "Chu"
-#Ex4
-movies = [1975, 2004, 2345, 1234,3255]
-
-puts movies[0]
-
-puts movies[1]
-puts movies[2]
-puts movies[3]
-puts movies[4]
+#Ex5
+puts 5 * 4 * 3 * 2 * 1
+puts 6 * 5 * 4 * 3 * 2 * 1
+puts 7 * 6 * 5 * 4 * 3 * 2 * 1
+puts 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
