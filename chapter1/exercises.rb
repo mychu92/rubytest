31e810f724529f6ba80f1cce8cc442bb0da8fd81
Maggie Chu <mychu92@gmail.com>
2016-02-04 19:27:23 +0800

    ex 1-3

--- chapter1/exercises.rb
+++ chapter1/exercises.rb
@@ -1,4 +1,15 @@
 #Ex1:
 "Maggie" + "Chu"
 #Ex2
-puts 2345/1000
+movies = {movie1: 1975,
+      movie2: 2004,
+      movie3: 2345,
+      movie4: 1234,
+      movie5: 3255
+}
+
+puts movies[:movie1]
+puts movies[:movie2]
+puts movies[:movie3]
+puts movies[:movie4]
+puts movies[:movie5]
