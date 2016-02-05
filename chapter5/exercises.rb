22f2ec282ba67b4690b3c69f0ec14eea7cb6af27
Maggie Chu <mychu92@gmail.com>
2016-02-05 21:35:35 +0800

    ex3

--- chapter5/exercises.rb
+++ chapter5/exercises.rb
@@ -2,8 +2,8 @@
 
 array = [1, 2, 3, 4, 5]
 x = 0
-array.each do |number|
-    puts "index: #{x} value: #{number}"
+array.each_with_index do |number, index|
+    puts "#{index + 1}. #{number}"
     x += 1
 end
 
