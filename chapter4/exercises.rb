bb5f125e3944f136d8604cb3e535ac867e3cd397
Maggie Chu <mychu92@gmail.com>
2016-02-05 18:01:44 +0800

    ex3

--- chapter4/exercises.rb
+++ chapter4/exercises.rb
@@ -1,9 +1,15 @@
-def caps(phrase)
-    if (phrase.length > 10)
-    phrase = phrase.upcase
-    else string
+#ex 3
+def observe (num)
+    if (num >= 0 && num <= 50)
+        puts "number is between 0 and 50"
+    elsif (num >= 51 && num <= 100)
+        puts "number is between 51 and 100"
+        
+    elsif (num > 100)
+    puts "number is above 100"
+    else puts "number is not valid"
     end
-    puts phrase
 end
 
-caps ("Hello world")
+num = gets.chomp.to_i
+observe(num)
