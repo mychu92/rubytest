2975a333076781ae70e1c8271bfa73a768296ceb
Maggie Chu <mychu92@gmail.com>
2016-02-06 15:02:13 +0800

    ch7 ex

--- chapter7/exercises.rb
+++ chapter7/exercises.rb
@@ -1,5 +1,19 @@
-#ex 5
+#ex 6
 
-hash1 = {one: 1, two: 2}
+words =  ['demo', 'none', 'tied', 'evil', 'dome', 'mode', 'live',
+'fowl', 'veil', 'wolf', 'diet', 'vile', 'edit', 'tide',
+'flow', 'neon']
 
-puts hash1.has_value?(9)
+result = {}
+
+words.each do |word|
+    key = word.split(//).sort.join
+    if result.has_key?(key)
+        result[key].push(word)
+    else result[key] = [word]
+    end
+end
+
+result.each do |k, v|
+p v
+end
